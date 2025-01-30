e001c0748f564d5c37d8c860e4ca794a287fa1fc
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2025-01-30 17:40:11 +0100

    Set shake presented console

--- Sources/SwiftPyConsole/SwiftPyConsole.swift
+++ Sources/SwiftPyConsole/SwiftPyConsole.swift
@@ -2,12 +2,39 @@
 // https://docs.swift.org/swift-book
 
 import DebugTools
+import SwiftUI
 
 @MainActor
 public final class SwiftPyConsole {
     public static let store = PythonLogStore()
     
+    @available(iOS 17.0, *)
     public static func initialize() {
         DebugTools.initialize(store: store)
+        
+        DebugTools.shakePresentedConsole = {
+            let view = GeometryReader { proxy in
+                let isPresented = proxy.size.height > 44
+                
+                Group {
+                    if isPresented {
+                        PythonConsoleView()
+                            .safeAreaInset(edge: .top, spacing: 0) {
+                                Rectangle()
+                                    .fill(.ultraThinMaterial)
+                                    .frame(height: 20)
+                            }
+                    } else {
+                        Text(">>>")
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                            .padding(8)
+                            .background(.thinMaterial)
+                    }
+                }.animation(.default, value: isPresented)
+            }
+            
+            return ConsoleViewController(base: UIHostingController(rootView: view))
+        }
     }
 }
