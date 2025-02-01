136fa476647bc83b14cfffc525208ac26380546d
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2025-02-01 14:42:14 +0100

    Set minimum compatible SwiftPy version

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
                  targets: ["SwiftPyConsole"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/felfoldy/SwiftPy", from: "0.2.0"),
+        .package(url: "https://github.com/felfoldy/SwiftPy", from: "0.2.3"),
         .package(url: "https://github.com/felfoldy/DebugTools", from: "0.4.0"),
         .package(url: "https://github.com/raspu/Highlightr", from: "2.2.1"),
     ],
